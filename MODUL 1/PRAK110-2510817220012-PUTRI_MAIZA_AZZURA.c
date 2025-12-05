b255ef3eb5a0c55df3a21d4f80e2d15dcd0751a9
Putri Maiza <114493671+2ZURE@users.noreply.github.com>
2025-12-05 12:31:02 +0700

    Add files via upload

---
+++ MODUL 1/PRAK110-2510817220012-PUTRI_MAIZA_AZZURA.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float alas = 5;
+    float tinggi = 12;
+    double hipotenusa = pow(((alas * alas) + (tinggi * tinggi)), 0.5);
+
+    printf("\nDiketahui : \n");
+    printf("Alas = %.0f cm \n", alas);
+    printf("Tinggi = %.0f cm \n", tinggi);
+    
+    printf("\nJawab : \n");
+    printf("Sisi A = %.0f cm \n", tinggi);
+    printf("Sisi B = %.0f cm \n", hipotenusa);
+    printf("Sisi C = %.0f cm \n", alas);
+    printf("Keliling = %.0f cm \n", alas + tinggi + hipotenusa);
+    printf("Luas = %.0f cm \n", (0.5 * alas * tinggi));
+    
+    return 0; 
+}
---
+++ MODUL 1/PRAK110-2510817220012-PUTRI_MAIZA_AZZURA.py
@@ -0,0 +1,14 @@
+alas = 5
+tinggi = 12
+hipotenusa = ((alas * alas) + (tinggi * tinggi)) ** 0.5
+
+print("Diketahui :")
+print("Alas = %.0f cm" % alas)
+print("Tinggi = %.0f cm" % tinggi)
+
+print("\nJawab :")
+print("Sisi A = %.0f cm" % tinggi)
+print("Sisi B = %.0f cm" % hipotenusa)
+print("Sisi C = %.0f cm" % alas)
+print("Keliling = %.0f cm" % (alas + tinggi + hipotenusa))
+print("Luas = %.0f cm" % (0.5 * alas * tinggi))
